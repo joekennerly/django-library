e6be7aab3387f45b08350117ac95d89bde785fe0
joekennerly <joseph.kennerly@gmail.com>
2019-09-20 14:39:19 -0500

    added detail view, edit, delete, navbar, and a base template for all others

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.urls import path
 from .views import *
 
 app_name = "libraryapp"
@@ -6,8 +7,12 @@ app_name = "libraryapp"
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^books$', book_list, name='books'),
+    url(r'^book/form$', book_form, name='book_form'),
+    url(r'^books/(?P<book_id>[0-9]+)/form$', book_edit_form, name='book_edit_form'),
+    path('books/<int:book_id>/', book_details, name='book'),
     url(r'^librarians$', list_librarians, name='librarians'),
     url(r'^libraries$', list_libraries, name='libraries'),
+    url(r'^libraries/form$', library_form, name='library_form'),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^logout/$', logout_user, name='logout')
 ]
--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -1,5 +1,8 @@
+from .home import home
 from .books.list import book_list
+from .books.details import book_details
+from .books.form import book_form, book_edit_form
 from .librarians.list import list_librarians
 from .libraries.list import list_libraries
-from .home import home
 from .auth.logout import logout_user
+from .libraries.form import library_form
