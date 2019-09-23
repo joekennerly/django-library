41f21d6a77d4a625ba3e8afcc77ef913b51f0885
joekennerly <joseph.kennerly@gmail.com>
2019-09-23 09:48:57 -0500

    details for all

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -6,3 +6,6 @@ from .librarians.list import list_librarians
 from .libraries.list import list_libraries
 from .auth.logout import logout_user
 from .libraries.form import library_form
+from .libraries.details import library_details
+from .librarians.details import librarian_details
+
